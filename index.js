f605fbb5eccba6bc3e68dedd68a401ff45f16557
Hasanat902 <abulhasanat902@gmail.com>
2022-09-26 11:47:32 +0600

    create available api

--- index.js
+++ index.js
@@ -27,6 +27,22 @@ async function run(){
             res.send(services);
         });
 
+        app.get('/available', async(req, res) => {
+            const date = req.query.date;
+            const services = await serviceCollection.find().toArray();
+            const query = {date: date};
+            const bookings = await bookingCollection.find(query).toArray();
+
+            services.forEach(service => {
+              const serviceBookings = bookings.filter(book => book.treatment === service.name);
+              const bookedSlots = serviceBookings.map(book => book.slot);
+              const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+              service.slots = available;
+            })
+
+            res.send(services);
+        })
+
         app.post('/booking', async(req, res) => {
             const booking = req.body;
             const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient};
@@ -35,7 +51,7 @@ async function run(){
               return res.send({success: false, booking: exists});
             }
             const result = await bookingCollection.insertOne(booking);
-            res.send({success: true, result});
+            return res.send({success: true, result});
         })
        
     }
